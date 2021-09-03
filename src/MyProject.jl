cc2e7e98e3b5a7285551cab488f54ed823e88678
Jesse Perla <jesseperla@gmail.com>
2021-09-02 17:17:54 -0700

    Added additional function

--- src/MyProject.jl
+++ src/MyProject.jl
@@ -3,10 +3,14 @@ module MyProject
 using Expectations, Distributions
 
 function foo(μ = 1., σ = 2.)
-    println("Modified foo definition")
+    println("Modified foo definition again")
     d = Normal(μ, σ)
     E = expectation(d)
     return E(x -> sin(x))
+end
+
+function bar()
+    return 10
 end
 
 export foo
