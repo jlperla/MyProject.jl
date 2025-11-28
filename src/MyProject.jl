c99ce7199990a80a02cff13c3e6d767b73679731
Jesse Perla <jesseperla@gmail.com>
2025-11-28 13:16:02 -0800

    Added cdf support

--- src/MyProject.jl
+++ src/MyProject.jl
@@ -7,6 +7,11 @@ function norm_pdf_val(x, mu = 1.0, sigma = 2.0)
     return pdf(d, x)
 end
 
-export norm_pdf_val
+function norm_cdf_val(x, mu = 1.0, sigma = 2.0)
+    d = Normal(mu, sigma)
+    return cdf(d, x)
+end
+
+export norm_pdf_val, norm_cdf_val
 
 end
