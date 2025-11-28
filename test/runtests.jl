b6899cc1f4bce08895f510eacc602aca61cfa9ac
Jesse Perla <jesseperla@gmail.com>
2025-11-28 09:47:01 -0800

    Use approx instead of ==

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,5 @@ using Test
 using Distributions
 
 @testset "MyProject.jl" begin
-    @test norm_pdf_val(1.0) == pdf(Normal(1.0, 2.0), 1.0)
+    @test norm_pdf_val(1.0) ≈ pdf(Normal(1.0, 2.0), 1.0)
 end
